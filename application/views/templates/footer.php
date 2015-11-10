2b078cca92b0aa7df654a61136507f3bd8785980
mlucero <rml2287@gmail.com>
2015-11-09 23:03:02 -0300

    Añadiendo base para responsive

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -3,5 +3,8 @@
 	
 </footer>
 <!-- footer -->
+
+<script src="<?php echo base_url()?>assetics/js/jquery-2.1.4.min.js" type="text/javascript" charset="utf-8"></script>
+<script src="<?php echo base_url()?>assetics/vendors/bootstrap-3.3.5-dist/js/bootstrap.min.js" type="text/javascript" charset="utf-8" ></script>
 </body>
 </html>
--- application/views/templates/head.php
+++ application/views/templates/head.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<title></title>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assetics/vendors/bootstrap-3.3.5-dist/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assetics/css/style.css">
 </head>
 <body>
