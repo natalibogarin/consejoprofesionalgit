9e68ef0f656ea6e9ade2f3554ca651e3d5a8da49
Natalia <natali.bogarin@gmail.com>
2015-11-15 18:51:44 -0300

    merge problems solved

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -3,14 +3,8 @@
 	
 </footer>
 <!-- footer -->
-<<<<<<< HEAD
-<script src="<?php echo base_url();?>assetics/js/jquery-2.1.4.min.js" type="text/javascript" charset="utf-8"></script>
-<script src="<?php echo base_url();?>assetics/vendors/bootstrap-3.3.5-dist/js/bootstrap.js" type="text/javascript" charset="utf-8"></script>
-</body></html>
-=======
 
 <script src="<?php echo base_url()?>assetics/js/jquery-2.1.4.min.js" type="text/javascript" charset="utf-8"></script>
 <script src="<?php echo base_url()?>assetics/vendors/bootstrap-3.3.5-dist/js/bootstrap.min.js" type="text/javascript" charset="utf-8" ></script>
 </body>
 </html>
->>>>>>> d9d8176ff2d8556363e6ee434849fdacae8c69d8
--- application/views/templates/nav.php
+++ application/views/templates/nav.php
@@ -1,13 +1,7 @@
 <!-- nav -->
-<<<<<<< HEAD
-<nav class="navbar navbar-inverse nati">
-	<ul>
-		<li><a href="" title="">uno</a></li>
-=======
 <nav class="navbar navbar-default nati">
 	<ul>
 		<li><a href="" title="">manuuuuuu</a></li>
->>>>>>> d9d8176ff2d8556363e6ee434849fdacae8c69d8
 	</ul>
 </nav>
 <!-- /nav -->
