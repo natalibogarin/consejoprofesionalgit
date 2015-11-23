5197f9fa72c90ad2890e598b98fbc40619adff2f
Natalia <natali.bogarin@gmail.com>
2015-11-23 16:22:11 -0300

    added autoridades

--- application/views/sitio/autoridades.php
+++ application/views/sitio/autoridades.php
@@ -3,32 +3,170 @@
 <div class="row">
 <div class="content col-xs-12 col-md-9 col-lg-9">
     <div class="row">
-        <img class="img-consejo" src="<?php echo base_url()?>assetics/img/consejo_abogados0.jpg" width="90%" align="middle" alt="Consejo Profesional de Abogados y Procuradores de Resistencia">
         <div class="header-consejo">
             <object class="header-icon" type="image/svg+xml" data="<?php echo base_url()?>assetics/img/icons/white/consejo-icon.svg"></object>
             <h4 class="header-title">Consejo Profesional de Abogados y Procuradores de Resistencia</h4>
         </div>
-        <article class="detail-consejo">
-            Fundado el 03 de noviembre de 1962. Los fines fundamentales del Consejo son: 
-            <ul class="list-consejo">
-                <li>La defensa de los intereses profesionales de sus miembros;</li>
-                <li>Velar por el mantenimiento de la ética profesional;</li>
-                <li>Promover la mayor ilustración en el ejercicio profesional de la abogacía. </li>
-            </ul>
-            En pos de la consecución de sus fines, y en su permanente actividad como entidad gremial, 
-            a lo largo de éstos años ha realizado numerosas actividades académicas en el ámbito local 
-            y nacional con participación activa de sus socios en Congresos, Conferencias Nacionales, 
-            reuniones nacionales de carácter científico y jornadas de capacitación. 
-            <br>
-            El CONSEJO PROFESIONAL DE ABOGADOS Y PROCURADORES busca la unidad de todos los profesionales del 
-            derecho mediante el Sistema Colegial Público, lo que va a significar garantía para el justiciable 
-            de que el profesional que lo atienda sea idóneo, tanto moral como técnicamente, ya que, con el control 
-            de la matrícula  y los tribunales de disciplina, se garantiza la mejor defensa de los derechos de quienes  representa.
-            <br>
-            <ul class="list-consejo">
-                <b><li style="list-style:none;">Reuniones todos los jueves a las 21 hs. en la Sede del Consejo de Abogados.</li></b>
-            </ul>
-        </article>
+        <h3 class="header-autoridades">Autoridades</h3>
+        <div class="row">
+            <article class="col-xs-12 col-sm-2 col-lg-2"></article>
+            <article class="col-xs-12 col-sm-4 col-lg-4">
+                <img class="autoridades" src="<?php echo base_url()?>assetics/img/img_presidente.png" alt="">
+                <div class="table-responsive">
+                    <table class="table">
+                        <tr class="table-autoridades">
+                             <td class="table-cargo">PRESIDENTE</td>
+                             <td>Dr. Hilario José BISTOLETTI</td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="table-responsive">
+                    <table class="table">
+                        <tr class="table-autoridades">
+                             <td class="table-cargo">SECRETARIO</td>
+                             <td>Dr. José Miguel VIGIER</td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="table-responsive">
+                    <table class="table">
+                        <tr class="table-autoridades">
+                             <td class="table-cargo">TESORERO</td>
+                             <td>Dr. Ricardo José URTURI</td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="table-responsive">
+                    <table class="table">
+                        <thead class="table-cargo">
+                            <tr>
+                                <td>VOCALES TITULARES</td>
+                            </tr>
+                        </thead>
+                        <tr>
+                            <td class="table-autoridades">
+                                <div class="table-vocales"> Dr. Oscar Alejandro CLEMENTE GUTIERREZ</div>
+                                <div class="table-vocales"> Dr. Francisco Rolando ORTIZ</div>
+                                <div class="table-vocales"> Dra. Patricia Alejandra PETRAY</div>
+                                <div class="table-vocales"> Dra. Elisa E. FERNANDEZ ASELLE</div>
+                                <div class="table-vocales"> Dra. Debora Ruth YAYA</div>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="table-responsive">
+                    <table class="table">
+                        <thead class="table-cargo">
+                            <tr>
+                                <td>REVISORES DE CUENTA TITULARES</td>
+                            </tr>
+                        </thead>
+                        <tr>
+                            <td class="table-autoridades">
+                                <div class="table-vocales"> Dra. Susana Marta WOLCOFF</div>
+                                <div class="table-vocales"> Dra. Rosa MARINICH</div>
+                                <div class="table-vocales"> Dr. Avelino Mario Daniel FERNANDEZ</div>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="table-responsive">
+                    <table class="table">
+                        <thead class="table-cargo">
+                            <tr>
+                                <td>REPRESENTANTES TITULARES ANTE LA FACA</td>
+                            </tr>
+                        </thead>
+                        <tr>
+                            <td class="table-autoridades">
+                                <div class="table-vocales"> Dr. José Alejandro SANCHEZ</div>
+                                <div class="table-vocales"> Dr. Benjamín Edgardo KAPEICA</div>
+                            </td>
+                        </tr>
+                    </table>
+                </div><br/>
+            </article>
+            <article class="col-xs-12 col-sm-4 col-lg-4">
+                <img class="autoridades" src="<?php echo base_url()?>assetics/img/img_vicepresidente.png" alt="">
+                <div class="table-responsive">
+                    <table class="table">
+                        <tr class="table-autoridades">
+                             <td class="table-cargo">VICEPRESIDENTE</td>
+                             <td>Dra. Sonia Cristina SEBA</td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="table-responsive">
+                    <table class="table">
+                        <tr class="table-autoridades">
+                             <td class="table-cargo">PRO SECRETARIA</td>
+                             <td>Dra. Aida Elisa BRISIGHELLI</td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="table-responsive">
+                    <table class="table">
+                        <tr class="table-autoridades">
+                             <td class="table-cargo">PRO TESORERO</td>
+                             <td>Dr. Aldo Esteban SANCHEZ</td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="table-responsive">
+                    <table class="table">
+                        <thead class="table-cargo">
+                            <tr>
+                                <td>VOCALES SUPLENTES</td>
+                            </tr>
+                        </thead>
+                        <tr>
+                            <td class="table-autoridades">
+                                <div class="table-vocales"> Dra. Mirian Rosana RAMIREZ</div>
+                                <div class="table-vocales"> Dr. Rodrigo Martín GONZALEZ</div>
+                                <div class="table-vocales"> Dra. Patricia Elena GUITART</div>
+                                <div class="table-vocales"> Dr. Edgardo Victor MORBIDONI</div>
+                                <div class="table-vocales"> Dr. Marco Antonio MOLERO</div>
+                                <div class="table-vocales"> Dr. Gustavo Javier LOPEZ</div>
+                                <div class="table-vocales"> Dr. Juan Manuel BARRANCO MENA</div>
+                                <div class="table-vocales"> Dr. Manuel Agustín ROMERO</div>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="table-responsive">
+                    <table class="table">
+                        <thead class="table-cargo">
+                            <tr>
+                                <td>REVISORES DE CUENTA SUPLENTES</td>
+                            </tr>
+                        </thead>
+                        <tr>
+                            <td class="table-autoridades">
+                                <div class="table-vocales"> Dr. Jorge Daniel FORNIES</div>
+                                <div class="table-vocales"> Dr. Jose Alvis WETTSTEIN</div>
+                                <div class="table-vocales"> Dra. Paulina VILLALBA</div>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="table-responsive">
+                    <table class="table">
+                        <thead class="table-cargo">
+                            <tr>
+                                <td>REPRESENTANTES SUPLENTES ANTE LA FACA</td>
+                            </tr>
+                        </thead>
+                        <tr>
+                            <td class="table-autoridades">
+                                <div class="table-vocales"> Dra. María Cecilia BARONI</div>
+                                <div class="table-vocales"> Dr. Pedro Alfredo REGUEIRO</div>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+            </article> 
+            <article class="col-xs-12 col-sm-4 col-lg-4"></article>
+        </div>
     </div>
     <div class="row-fluid">
         <article class="logo-footer col-sm-3 col-lg-3"> </article>
